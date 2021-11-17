2244ecab9905d26fb38b4d08772412504122f556
ivg97 <gavr97.97@mail.ru>
2021-11-17 22:06:14 +0300

    lesson 2 + html +css

--- todo/frontend/src/App.js
+++ todo/frontend/src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import UserList from "./components/User";
+import axios from "axios";
+import {Footer} from "./components/Footer";
+import Menu from "./components/Menu";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            'users': []
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://127.0.0.1:8000/api/users/').then(
+            response => {
+                const users = response.data
+                this.setState({
+                    'users': users
+                })
+            }
+        ).catch(error => console.log(error))
+
+        // const users = [
+        //     {
+        //         'username': 'i',
+        //         'first_name': 'ivg',
+        //         'last_name': 'gavr',
+        //         'email': '@'
+        //     }
+        // ]
+        // this.setState({
+        //     'users': users
+        // })
+    }
+
+    render() {
+        return (
+            <div>
+                <Menu/>
+                <UserList users={this.state.users}/>
+                <Footer/>
+            </div>
+
+        );
+
+    }
+
 }
 
 export default App;
